96897a282b8f2d765606c659855154e4c18b07f1
motoha <motoha2010@gmail.com>
2024-03-25 08:15:14 +0700

    getx add to cart part 3

--- lib/app/modules/ecommerce/controllers/ecommerce_controller.dart
+++ lib/app/modules/ecommerce/controllers/ecommerce_controller.dart
@@ -9,15 +9,25 @@ class EcommerceController extends GetxController with StateMixin {
     var getId = cart.where((Cart c) => c.id == id).toList();
 
     if (getId.isNotEmpty) {
-      getId[0].price = price + getId[0].price;
+      getId[0].total = getId[0].total + getId[0].price;
       getId[0].count = count + getId[0].count;
 
       update();
       change(cart, status: RxStatus.success());
     } else {
-      cart.add(Cart(id: id, name: name, price: price, total: 0, count: 1));
+      cart.add(Cart(id: id, name: name, price: price, total: price, count: 1));
     }
     update();
     change(cart, status: RxStatus.success());
+  }
+
+  delete(id, price) {
+    var getId = cart.where((Cart c) => c.id == id).toList();
+    if (getId.isEmpty) {
+      cart.removeWhere((item) => item.id == id);
+    } else {
+      getId[0].count = getId[0].count - 1;
+      getId[0].total = getId[0].total - price;
+    }
   }
 }
--- lib/app/modules/ecommerce/views/cart_page.dart
+++ lib/app/modules/ecommerce/views/cart_page.dart
@@ -14,29 +14,64 @@ class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Text("YOur cart"),
-          Divider(),
-          //loop cart item
-          for (var i = 0; i < con.cart.length; i++)
-            Row(
-              children: [
-                Text("${con.cart[i].name}"),
-                SizedBox(width: 10),
-                Text("X"),
-                SizedBox(
-                  width: 10,
-                ),
-                Text("${con.cart[i].count.toString()}"),
-                Spacer(),
-                Text("${con.cart[i].price.toString()}")
-              ],
-            )
-        ],
-      )),
-    );
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Obx(() => Column(
+                children: [
+                  Text("YOur cart"),
+                  Divider(),
+                  //loop cart item
+                  for (var i = 0; i < con.cart.length; i++)
+                    Row(
+                      children: [
+                        Text("${con.cart[i].name}"),
+                        SizedBox(width: 10),
+                        Text("X"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                con.addTocart(con.cart[i].id, con.cart[i].name,
+                                    con.cart[i].price, 1);
+                              });
+                            },
+                            child: Text("+")),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("${con.cart[i].count.toString()}"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                con.delete(con.cart[i].id, con.cart[i].price);
+                              });
+                            },
+                            child: Text("-")),
+                        Spacer(),
+                        Text("${con.cart[i].total.toString()}")
+                      ],
+                    ),
+                  ElevatedButton(
+                      onPressed: () {
+                        var map = {};
+                        for (var i = 0; i < con.cart.length; i++) {
+                          map.addAll({
+                            "id[$i]": con.cart[i].id,
+                            "product_name[$i]": con.cart[i].name,
+                            "total[$i]": con.cart[i].total,
+                            "count[$i]": con.cart[i].count
+                          });
+                        }
+                        print(map);
+                      },
+                      child: Text("Checkout"))
+                ],
+              )),
+        ));
   }
 }
